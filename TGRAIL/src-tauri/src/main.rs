38e5a58af4aaf3f2488afa8b0eb4fb85628d3bd6
Luna Kilborn <leland.kilborn@hotmail.com>
2022-05-03 17:37:07 -0400

    Implement basic path processing functions
    I kicked off the processing by implementing some basic  functions relating to readability. The implemented functions were
    * converting absolute position to segment code
    * determine bounding box of a given path
    * calculate the aspect ratio of a given path

--- TGRAIL/src-tauri/src/main.rs
+++ TGRAIL/src-tauri/src/main.rs
@@ -1,17 +1,20 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
+
+mod char_rec;
 
 #[tauri::command]
 fn log_points(x_pts: Vec<i32>, y_pts: Vec<i32>) {
-  println!("{:?}", x_pts);
-  println!("{:?}", y_pts);
+    println!("{:?}", x_pts);
+    println!("{:?}", y_pts);
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![log_points])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    println!("{}", char_rec::Character::pos_to_seg(16, 16, 10, 7));
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![log_points])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
